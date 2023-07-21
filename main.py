1a309a534f2d88d3848701a7fe22610e845c3fbd
Prince Bhargava <princebhargavapr@gmail.com>
2023-07-22 03:14:49 +0530

    implement exit button and theme

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import PySimpleGUI as sg
 from convertor import convert
+sg.theme("Black")
 label1 = sg.Text("Enter feet    ")
 input1 = sg.Input(key="feet")
 
@@ -8,16 +9,19 @@ input2 = sg.Input(key="inches")
 
 convert_button = sg.Button("Convert")
 output_label = sg.Text(key="output")
+exit_button = sg.Button("Exit")
 
 window = sg.Window("Converter",
                    layout = [[label1,input1],
                              [label2,input2],
-                             [convert_button,output_label]])
+                             [convert_button,output_label,exit_button]])
 while True:
     event,value = window.read()
     print((event,value))
     if event == sg.WINDOW_CLOSED:
         exit()
+    if event == "Exit":
+        break
     feet = float(value["feet"])
     inches = float(value["inches"])
 
